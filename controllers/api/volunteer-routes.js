d5123d1296b044b353f85bfc54377c617aa1aa27
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-03-23 21:02:03 -0700

    setting endpoints for routes

--- controllers/api/volunteer-routes.js
+++ controllers/api/volunteer-routes.js
@@ -3,17 +3,51 @@ const withAuth = require('../../utils/auth')
 const sequelize = require('../../config/connection');
 const { Volunteer } = require('../../models');
 
-//get all volunteers
+//get all volunteers via /api/volunteer endpoint
 router.get('/', (req, res) => {
     Volunteer.findAll({
         attributes: {
             exclude: ['password']
         }
     })
-        .then(volunteerData = res.json(volunteerData))
+        .then(volunteerData => {
+            console.log('router.get inside home-routes.js');
+            res.json(volunteerData)
+        })   
         .catch(err => {
             res.status(500).json(err);
         })
 });
+
+//get volunteer by id
+router.get('/:v_id', (req, res) => {
+    Volunteer.findOne({
+        attributes: {
+            exclude: ['password']
+        },
+        where: {
+            v_id: req.params.v_id
+        }
+    })
+        .then(volunteerData => {
+            console.log('router.get inside home-routes.js');
+            res.json(volunteerData)
+        })   
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+//Create a new volunteer 
+router.post('/', (req, res) => {
+    Volunteer.create({
+        username:req.body.username,
+        password:req.body.password
+    })
+    .then(volunteerData => {
+
+    })
+})
+
 
 module.exports = router;

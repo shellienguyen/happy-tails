d7815ab707f6a65fa1f2f73bee60f4eb369f8837
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-03-24 19:37:41 -0700

    listed attributes for canine-route

--- controllers/api/canine-routes.js
+++ controllers/api/canine-routes.js
@@ -29,32 +29,6 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     })
-    // Canine.findAll({
-    //     attributes: [
-    //         'c_id', 
-    //         'c_name', 
-    //         'c_demeanor', 
-    //         'has_walked_am', 
-    //         'has_walked_pm', 
-    //         'has_potty_am', 
-    //         'has_potty_pm', 
-    //         'k_id',
-    //     ],
-    //     include: [{
-    //         model: Volunteer,
-    //         attributes: ['username']
-    //     }]
-    // })
-    //     .then(dbCanineData => {
-    //         console.log('router.get inside home-routes.js');
-    //         res.json(dbCanineData);
-
-    //     })
-    //     .catch(err => {
-    //         console.log("Error in router.get in home-route.js");
-    //         console.log(err);
-    //         res.status(500).json(err);
-    //     });
 });
 
 //get canine by id

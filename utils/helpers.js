bcdde1964c60db380ef404836fef341012ec5831
sidonie <shelliednnguyen@gmail.com>
2021-03-30 01:47:34 -0600

    happy_tail_am and happy_tail_pm

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,55 +1,45 @@
-module.exports = {
+let allHelpers = {
   format_date: date => {
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
       date
     ).getFullYear()}`;
   },
 
-  // format_plural: (word, amount) => {
-  //   if (amount !== 1) {
-  //     return `${word}s`;
-  //   }
-
-  //   return word;
-  // },
-
   shift_change: () => {
     // let currentHour = moment().hour();
     let today = new Date();
     let currentHour = today.getHours();
 
-    console.log(currentHour);
     if (currentHour >= 0 && currentHour <= 11) {
-      console.log('inside here')
-        return true;
+      return true;
     }
-    console.log('hello')
+
     return false;
-    
+
   },
 
   // check_box: () => {
-    // if dog has_walked or has_pottied then load that status into checkboxes on single-dog page using "checked" attribute
-   
+  // if dog has_walked or has_pottied then load that status into checkboxes on single-dog page using "checked" attribute
+
   // },
-  
-  happy_tail_am: () => {
+
+  happy_tail_am: (has_walked_am, has_potty_am) => {
     // if dog has_walked AND has_pottied then change status-emoji from sad face to happy face 
-    if (has_walked_am && has_potty_am && shift_change) {
-      
+    if (has_walked_am && has_potty_am && allHelpers.shift_change()) {
       return true;
     }
+
     return false;
   },
 
-  happy_tail_pm: (has_walked_pm) => {
+  happy_tail_pm: (has_walked_pm, has_potty_pm) => {
     // if dog has_walked AND has_pottied then change status-emoji from sad face to happy face 
-    if (has_walked_pm) {
-      console.log('say something')
+    if (has_walked_pm && has_potty_pm && !allHelpers.shift_change()) {
       return true;
-      
-    }
-    console.log('whatever')
+    };
+
     return false;
   }
 };
+
+module.exports = allHelpers;
